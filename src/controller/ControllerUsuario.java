667d75b7d8e5a40d60c3fa4301650aa3546a3c25
Géssica <Gssica@Gessica>
2015-12-18 16:50:52 -0300

    controller usuario

---
+++ src/controller/ControllerUsuario.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.Usuario;
+import settings.Facade;
+
+/**
+ *
+ * @author Géssica
+ */
+public class ControllerUsuario {
+    
+    private Usuario usuario = new Usuario();
+    private final Facade facade = Facade.getInstance();
+}
