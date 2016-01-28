ec53fc1bf81dfdb9f89d6328518bf0de02fad326
Spider <>
2016-01-28 11:17:21 -0300

    implementação do envio de email.

--- src/util/MyEmail.java
+++ src/util/MyEmail.java
@@ -3,6 +3,7 @@ package util;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
+import org.apache.commons.mail.SimpleEmail;
 
 /**
  *
@@ -16,50 +17,56 @@ public class MyEmail {
     private int port;
 
     public MyEmail() {
-//        sender = "iuri.raiol@icen.ufpa.br";
-//        password = "iuri23";
-//        host = "smtp.ufpa.br";
+        sender = "iuri.raiol@icen.ufpa.br";
+        password = "iuri23";
+        host = "smtp.ufpa.br";
+        port = 25;
+        
+//        sender = "luiz-odl@hotmail.com";
+//        password = "motorola1986";
+//        host = "smtp.live.com";
 //        port = 587;
         
-        sender = "projetospiderdar@gmail.com";
-        password = "5p1d3rd4r";
-        host = "smtp.gmail.com";
-        port = 465;
+//        sender = "projetospiderdar@gmail.com";
+//        password = "5p1d3rd4r";
+//        host = "smtp.gmail.com";
+//        port = 465;
     }
 
     public void SendNewPassword(String address, String name, String newPassword) {
         String subject = "Nova senha Spider-DAR";
-        String message = "<html>"
-                + "Olá " + name + "<br><br>"
-                + "Sua nova senha é: " + newPassword
-                + "</html>";
-        sendEmail(address, subject, message);
+//        String message = "<html>"
+//                + "Olá " + name + "<br><br>"
+//                + "Sua nova senha é: " + newPassword
+//                + "</html>";
+        sendEmail(address, subject, "Teste");
     }
 
     private void sendEmail(String address, String subject, String message) {
         try {
-            HtmlEmail email = new HtmlEmail();
+            SimpleEmail email = new SimpleEmail();
             
             email.setDebug(true); 
             email.setHostName(host);
             email.setSmtpPort(port);
             
-            email.setAuthenticator(new DefaultAuthenticator(sender, password));
-            email.setSSLOnConnect(true);
-            email.setStartTLSEnabled(true); 
-            email.setStartTLSRequired(true);
+            email.setAuthenticator(new DefaultAuthenticator(sender, password));  
+//            Google
+//            email.setSSLOnConnect(true);
+//            email.setSSLCheckServerIdentity(true);
+
+//            OutLook
+//            email.setStartTLSEnabled(true); 
+//            email.setStartTLSRequired(true);
             
             email.setFrom(sender);
             email.setSubject(subject);
             email.addTo(address);
-            email.setHtmlMsg(message);
+            email.setMsg(message);
             email.send();
         } catch (EmailException error) {
             error.printStackTrace();
         }
     }
 
-    private void setAuthenticator(DefaultAuthenticator defaultAuthenticator) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
