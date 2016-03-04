e492f47e8af74753b0899ab82b15c7ddfdb038a5
Géssica Pinheiro <>
2016-03-04 13:07:38 -0300

    relatório

---
+++ src/controller/Relatorio.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.swing.text.Document;
+
+/**
+ *
+ * @author Géssica
+ */
+public class Relatorio {
+    
+    public void gerarRelatorio() throws IOException {
+        Document document = null;
+        OutputStream outputStream = null;
+        
+    }
+}
