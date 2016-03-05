60e6fe3b8776023143b3e03b999f47f25ad66da9
Géssica <Gssica@gessica>
2016-03-05 00:06:00 -0300

    relatórios

--- src/controller/Relatorio.java
+++ src/controller/Relatorio.java
@@ -1,18 +1,44 @@
 package controller;
 
-import java.io.IOException;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
-import javax.swing.text.Document;
-
-/**
- *
- * @author Géssica
- */
-public class Relatorio {
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
     
-    public void gerarRelatorio() throws IOException {
-        Document document = null;
-        OutputStream outputStream = null;
-        
-    }
-}
+    public class Relatorio {
+        public static void main(String[] args) throws Exception {
+            
+            Document doc = null;
+            OutputStream os = null;
+
+	        try {
+	            //cria o documento tamanho A4, margens de 2,54cm
+                    doc = new Document(PageSize.A4, 72, 72, 72, 72);
+
+	            //cria a stream de saída
+	            os = new FileOutputStream("out.pdf");
+	       
+	            //associa a stream de saída ao
+	            PdfWriter.getInstance(doc, os);
+
+	            //abre o documento
+	            doc.open();	 
+
+	            //adiciona o texto ao PDF
+	            Paragraph p = new Paragraph("Meu primeiro arquivo PDF!");
+	            doc.add(p);
+	 
+	        } finally {
+	            if (doc != null) {
+	                //fechamento do documento
+	                doc.close();
+	            }
+	            if (os != null) {
+	               //fechamento da stream de saída
+	               os.close();
+	            }
+	        }
+	    }
+	}
