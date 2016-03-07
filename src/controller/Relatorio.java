46fd05ea4bff95cd5dfbdf64e3bcd0bd8db68264
Géssica <Gssica@gessica>
2016-03-06 21:26:16 -0300

    relatório

--- src/controller/Relatorio.java
+++ src/controller/Relatorio.java
@@ -4,10 +4,14 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.IOException;
+import settings.KeepData;
+import util.Request;
     
     public class Relatorio {
         
@@ -15,8 +19,15 @@ import java.io.IOException;
             
             Document doc = null;
             OutputStream os = null;
+            Request request = new Request();
 
 	        try {
+                    
+                    //fontes
+                    Font fonte1 = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+                    Font fonte2 = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+                    Font fonte3 = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
+                    
 	            //cria o documento tamanho A4, margens de 2,54cm
                     doc = new Document(PageSize.A4, 72, 72, 72, 72);
 
@@ -29,9 +40,19 @@ import java.io.IOException;
 	            //abre o documento
 	            doc.open();	 
 
-	            //adiciona o texto ao PDF
-	            Paragraph p = new Paragraph("Teste!");
-	            doc.add(p);
+                    //titulo
+                    Paragraph p1 = new Paragraph("RELATÓRIO DE GESTÃO DE DECISÃO", fonte1);
+                    p1.setAlignment(Element.ALIGN_CENTER);
+                    p1.setSpacingAfter(20); 
+                    doc.add(p1);
+                    
+	            String nomeOrganizacao = KeepData.getData("Organizacao.nome");
+                    Paragraph p3 = new Paragraph("1. Organização: " + nomeOrganizacao, fonte2);
+                    doc.add(p3);
+                    
+                    String nomeProblema = KeepData.getData("Problema.nome");
+                    Paragraph p4 = new Paragraph("2. Problema: " + nomeProblema, fonte2);
+                    doc.add(p4);
 	 
 	        } finally {
 	            if (doc != null) {
